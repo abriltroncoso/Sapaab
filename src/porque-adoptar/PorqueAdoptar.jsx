5944b51dfed242bf301b88f8347aca52b73b89f7
abril troncoso <abrilpotitroncoso@gmail.com>
2025-11-15 23:31:17 -0300

    detalles mobile

--- src/porque-adoptar/PorqueAdoptar.jsx
+++ src/porque-adoptar/PorqueAdoptar.jsx
@@ -5,7 +5,7 @@ import imgC from '../assets/porque3.svg';
 import imgD from '../assets/porque4.svg';
 import imgE from '../assets/porque5.svg';
 export default function PorqueAdoptar() {
-    
+
     return(
         <div className="porque-adoptar">
             <div className="img-column left">
@@ -21,7 +21,7 @@ export default function PorqueAdoptar() {
                      Cuando adoptas, le das una segunda oportunidad a un ser lleno de amor. Cada callejerito ha superado grandes desafios y esta listo para encontrar un nuevo hogar.
                 </p>
                 <p>
-                    Adoptar es mucho mas que llevar un perro a casa: es salvar una vida y liberar un espacio en el Refugio para que otro callejito pueda ser rescatado. Es un acto de amor, responsabilidad y compromiso.
+                    Adoptar es mucho mas que llevar un perro a casa: es salvar una vida y liberar un espacio en el Refugio para que otro callejito pueda ser rescatado. <strong>Es un acto de amor, responsabilidad y compromiso.</strong> 
                 </p>
                 <button className="btn-quiero-adoptar">Adopta ahora</button>
             </div>
