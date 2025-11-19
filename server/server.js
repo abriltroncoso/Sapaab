4524ab89c86ba457ff208410c683c30e624bbffa
abril troncoso <abrilpotitroncoso@gmail.com>
2025-11-19 15:36:48 -0300

    url actualizada

--- server/server.js
+++ server/server.js
@@ -24,6 +24,4 @@ app.use("/api/perros", perroRoutes);
 app.use("/api/auth", authRoutes);
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-});
+app.listen(PORT, () => {});
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://sapaab.onrender.com/api";
 
 // Obtener el token del localStorage
 const getToken = () => {
